64be34084ef3d6bd8c84375a1771694c52bcb91e
Markus Gerber <markus.gerber@fau.de>
2024-08-27 14:28:09 +0200

    updated frequency + plots

--- zephyr/vnv_heap_benchmark/analysis/util/plot.py
+++ zephyr/vnv_heap_benchmark/analysis/util/plot.py
@@ -16,12 +16,15 @@ def set_gird(grid_offset: int, max_x: int, ax):
     ax.set_xticks(major_ticks)
     ax.set_xticks(minor_ticks, minor=True)
 
-def save_plot(name: str):
+def save_plot(name: str, save_asset: bool = False):
     plt.savefig(f"../figures/{name}.pdf", bbox_inches='tight')
 
     if "VNV_HEAP_THESIS_DIR" in os.environ:
         thesis_dir = os.environ["VNV_HEAP_THESIS_DIR"]
         plt.savefig(f"{thesis_dir}/figures/plot_{name}.pdf", bbox_inches='tight')
+
+    if save_asset:
+        plt.savefig(f"../../../assets/{name}_plot.svg", bbox_inches='tight')
 
 def set_theme(colors=3, skip=0, ignore=-1):
     sns.set_theme()
