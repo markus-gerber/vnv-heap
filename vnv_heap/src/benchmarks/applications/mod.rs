ca285e1576391453cd91337e13eb30c25a870464
Markus Gerber <markus.gerber@fau.de>
2025-02-23 15:50:23 +0100

    import

--- vnv_heap/src/benchmarks/applications/mod.rs
+++ vnv_heap/src/benchmarks/applications/mod.rs
@@ -1,3 +1,4 @@
 // mod mat_mul;
+mod key_value_store;
 mod event_queue;
-pub(super) use event_queue::EventQueueBenchmarkRunner;
+pub(super) use {event_queue::EventQueueBenchmarkRunner, key_value_store::KVSBenchmarkRunner};
